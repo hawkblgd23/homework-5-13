8e63a78cb4e6b46f9b4f4bff0ff75c1c08c2e08d
hawkblgd23 <50380120+hawkblgd23@users.noreply.github.com>
2019-05-13 13:58:31 -0500

    Add files via upload

---
+++ JSHW/review.js
@@ -0,0 +1,38 @@
+
+console.log('Fishing');
+var trading = 'money';
+var hedge = 'safe';
+console.log(trading,hedge);
+let numbers1 = - 15;
+let numbers2 = + 25;
+let numbers3 = numbers2* 2;
+let numbers4 = 100;
+console.log (numbers3);
+console.log (numbers1);
+console.log (numbers3);
+let equation1 = numbers2 + numbers1;
+console.log  (numbers4);
+console.log  (equation1);
+let fishinglist = ['minnows','liver','worms'];
+let numlist = [2,4,6,];
+var fishingnet = ('true');
+console.log(fishingnet);
+console.log(fishingnet);
+//boolean
+var fishingnet = (true);
+console.log(fishingnet);
+var fishingnet = true;
+console.log(fishingnet);
+console.log('fishinglist');
+console.log(numlist);
+//splice
+console.log(fishinglist.splice(2));
+
+fishinglist.push('goldfish','crickets');
+console.log(fishinglist);
+// string interpolation
+var fish = 5;
+var crappie = `best fish fish in town ${fish}`;
+console.log (crappie);
+//retrieve type of data
+console.log (typeof fishinglist);
